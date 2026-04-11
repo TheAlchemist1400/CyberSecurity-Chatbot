59d12b77a8cffc963eec316bc8a73c6e06f2ce12
TheAlchemist1400 <armando.moabi@gmail.com>
2026-04-12 01:28:21 +0200

    Added comments

--- Greeting.cs
+++ Greeting.cs
@@ -9,14 +9,20 @@ namespace Chatbot_Part1
 {
     internal class AudioPlayer
     {
-        try
+        public static void PlayGreeting()
         {
-            SoundPlayer greeting = new SoundPlayer(@"C:\Users\Armando Moabi\Documents\Rosebank\3rd semester 2026\PROG6221\Class acts\Chatbot Part1\05 - Party On Venus (Prod by by OmArr Josiah of Stoopid Robots");
-            greeting.PlaySync();
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("An error occured: " + ex.Message);
+            // Use the SoundPlayer class to "TRY" and play the audio file, Use try incase an error occurs it doesnt crash the program
+            try
+            {
+                // TODO : Change the audio to the correct one
+                SoundPlayer greeting = new SoundPlayer(@"C:\Users\Armando Moabi\Documents\Rosebank\3rd semester 2026\PROG6221\Class acts\Chatbot Part1\05 - Party On Venus (Prod by by OmArr Josiah of Stoopid Robots");
+                greeting.PlaySync();
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that may occur while trying to play the audio and return an error message to the user
+                Console.WriteLine("An error occured: " + ex.Message);
+            }
         }
     }
 }
---
+++ Logo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatbot_Part1
+{
+    internal class Logo
+    {
+        public static void DisplayLogo()
+        {
+            // Ascii art from https://www.asciiart.eu/text-to-ascii-art
+            Console.WriteLine(@"
+________      ___    ___ ________  _______   ________       
+|\   ____\    |\  \  /  /|\   __  \|\  ___ \ |\   __  \      
+\ \  \___|    \ \  \/  / | \  \|\ /\ \   __/|\ \  \|\  \     
+ \ \  \        \ \    / / \ \   __  \ \  \_|/_\ \   _  _\    
+  \ \  \____    \/  /  /   \ \  \|\  \ \  \_|\ \ \  \\  \|   
+   \ \_______\__/  / /      \ \_______\ \_______\ \__\\ _\   
+    \|_______|\___/ /        \|_______|\|_______|\|__|\|__|  
+             \|___|/                                         
+                                                             
+                                                             
+ ___  ___  ________  ___       __   ___  __    ________      
+|\  \|\  \|\   __  \|\  \     |\  \|\  \|\  \ |\   ____\     
+\ \  \\\  \ \  \|\  \ \  \    \ \  \ \  \/  /|\ \  \___|_    
+ \ \   __  \ \   __  \ \  \  __\ \  \ \   ___  \ \_____  \   
+  \ \  \ \  \ \  \ \  \ \  \|\__\_\  \ \  \\ \  \|____|\  \  
+   \ \__\ \__\ \__\ \__\ \____________\ \__\\ \__\____\_\  \ 
+    \|__|\|__|\|__|\|__|\|____________|\|__| \|__|\_________\
+                                                 \|_________|
+            CYBER HAWKS, a cyber security chatbot.");
+
+            // Restore the original console colors
+            Console.ResetColor();
+        }
+}
