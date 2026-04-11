e418943c60d90505fce293ac392072d6747a644a
TheAlchemist1400 <armando.moabi@gmail.com>
2026-04-12 01:13:53 +0200

    Add project files.

---
+++ Greeting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Media;
+using System.Threading.Tasks;
+
+namespace Chatbot_Part1
+{
+    internal class AudioPlayer
+    {
+        try
+        {
+            SoundPlayer greeting = new SoundPlayer(@"C:\Users\Armando Moabi\Documents\Rosebank\3rd semester 2026\PROG6221\Class acts\Chatbot Part1\05 - Party On Venus (Prod by by OmArr Josiah of Stoopid Robots");
+            greeting.PlaySync();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occured: " + ex.Message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NAudio.Wave;
+using System.Threading.Tasks;
+
+namespace Chatbot_Part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
